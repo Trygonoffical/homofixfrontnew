d8bed1171993e4bedbd9a5bb35ae3fd41702352d
Trygonoffical <trygontechnologies@gmail.com>
2023-12-26 22:32:24 +0530

    center homosextin

--- components/Homeservice.jsx
+++ components/Homeservice.jsx
@@ -97,7 +97,7 @@ const slugify = (text) => {
             // removeArrowOnDeviceType={["desktop", "superLargeDesktop"]}
             dotListClass="custom-dot-list-style"
             itemClass="carousel-item-padding-40-px"
-            
+            className="justify-center"
             >
 
             {/* {book.map((cat , idx) => 
