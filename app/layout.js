54b7662e8a1b775175627bdf1e9beb87978a8533
Trygonoffical <trygontechnologies@gmail.com>
2024-03-15 21:39:54 +0530

    google tag added 3

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,11 @@
 
 import Header from '@/components/Header'
+// import Script from 'next/script';
 import { AuthProvider } from '@/components/AuthContext'
 import { GoogleTagManager , GoogleAnalytics } from '@next/third-parties/google'
-// import googletag from './googletag'
-import Head from 'next/head'
+
+import Googletags from '@/components/metrics/Googletags'
+import MicrosoftClarity from '@/components/metrics/MicrosoftClarity'
 import { Footer } from '@/components/Footer'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
@@ -21,23 +23,10 @@ export default function RootLayout({ children }) {
   <>
   <html lang="en">
     {/* <googletag/> */}
-     <Head >
-     <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16494970140" />
-        
-        <script dangerouslySetInnerHTML={{__html:`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'AW-16494970140');`}} />
-          
-        
-        {/* <script dangerouslySetInnerHTML={{__html:`(function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y)})(window, document, "clarity", "script", "lgxt601tn5");`}} /> */}
-     </Head>
+     
         <AuthProvider>
-          
+            <Googletags />
+            <MicrosoftClarity />
             <body className={inter.className}>
               <Header />
               {children}
